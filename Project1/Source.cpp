3c456f214a3d9406600089f7aeafe8e2d5f74510
Alisa <momoirokutsyne@gmail.com>
2019-09-07 21:18:19 +0400

    Добавьте файлы проекта.

---
+++ Project1/Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+void main() {
+	cout << "Hello, World!\n";
+	cout << "This is my attempt to create a repository!\n";
+	cout << "It will be long and painful, but I will manage! Hehehe...";
+}
