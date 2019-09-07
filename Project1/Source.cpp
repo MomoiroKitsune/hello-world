c7433d5bcd446a93f3b558725ff826333461ae62
Alisa <momoirokutsyne@gmail.com>
2019-09-07 21:44:27 +0400

    hmmmmm

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -4,4 +4,5 @@ void main() {
 	cout << "Hello, World!\n";
 	cout << "This is my attempt to create a repository!\n";
 	cout << "It will be long and painful, but I will manage! Hehehe...";
+	cout << "hm what??"
 }
