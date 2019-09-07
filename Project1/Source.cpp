9d33865064e865233bde2398f6910ca7669c365d
Alisa <momoirokutsyne@gmail.com>
2019-09-07 21:49:18 +0400

    lol i'm understand, i mean hd

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -4,5 +4,4 @@ void main() {
 	cout << "Hello, World!\n";
 	cout << "This is my attempt to create a repository!\n";
 	cout << "It will be long and painful, but I will manage! Hehehe...\n";
-	cout << "(help, i dk wtf T-T)";
 }
